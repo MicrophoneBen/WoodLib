bc45cf6822247fa9de7ddf5134e470ff58930c02
Wood <liyao6@foxmail.com>
2018-03-05 09:12:43 +0800

    线性表链式存储结构之单链表类 LinkList 类 的设计要点：   1、因为前面顺序表的效率底下的问题（插入和删除需要移动大量元素），链表类就是为解决这个问题而诞生的；   2、单链表在设计上也是属于线性表的一种，所以 LinkList类 继承自 抽象类List（线性表类）；   3、LinkList类需要结点，故就有了 struct Node 内部类用来定义结点的、链表的操作最好有一个头结点来辅助，故就有了 m_header，链表需要有个变量记录长度，也就有了m_length变量；   4、实现链表插入、删除等函数时，有共同逻辑获取目标位置前一个结点的地址/指针，就将这部分逻辑单独设计为一个函数 position()；   5、position()设计中，犯了一个恶心的低级错误，将for循环的执行过程搞错；注意这个函数目的是返回目标位置的前一个结点的地址/指针；   6、结点的数据域是个泛型T，所以当存储是自定义类型时会在这个头结点成员m_header变量上触发T类型的构造函数，若T构造函数中有抛出异常，那么别人就会说是库的设计有Bug，这个问题就可以用借尸还魂的方法解决；   7、借尸还魂就是不直接用 Node定义头结点，而是创建一个和Node一样内存布局的头结点，保存next指针，想方设法废除数据域使其不是一个泛型类型不就OK了，反正头结点数据域有没有使用；   8、代码细节点：      Struct Nodt 内部类有泛型T，需要继承自Object；      m_header 为了保持和 Node m_header一样的内存布局，也继承自Object；      const函数中对头结点取地址操作编译器就认为是有可能改变这个成员的值的，这个时候使员mutable关键字；      析构函数直接复用 clear() 中的代码。

---
+++ LinkList.h
@@ -0,0 +1,208 @@
+// LinkList类 线性表链式存储结构之单链表类
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+
+#include "List.h"
+#include "Exception.h"
+
+#include "iostream"
+
+using namespace std;
+
+namespace WoodLib
+{
+
+template < typename T >
+class LinkList : public List<T>
+{
+private:
+    // 结点类型（内部类，C++中struct也是类）
+    // 因为可能需要涉及到T类型对象创建,那么就有可能需要new,故也要继承自Object
+    struct Node : public Object
+    {
+        T value;     // 数据域
+        Node* next;  // 指针域
+    };
+
+    // 由于m_header在定义时会调用T的构造函数。如果T的设计者在构造
+    // 函数中抛出异常，当LinkList<T> list时会产生异常。这时调用
+    // 者可能会怀疑是LinkList设计出了问题，而不会认为是T的问题，
+    // 因为他根本就没有手工定义T的对象（如：T t），为了防止这种现
+    // 象的出现，可“借尸还魂”，防止在定义m_header时调用T的构造函数。
+
+    // mutable Node m_header;  // 头结点
+
+    // “借尸还魂”:定义一个与Node内存模型完全一样的结构体，防止调
+    // 用T的构造函数（因为这只是个头结点，关心的是next指针域，而
+    // 不是数据域的东西）
+
+    // 为了保证和 Node m_header 一样的内存布局，也继承自Object
+    mutable struct : public Object
+    {
+        char reserved[sizeof(T)];
+        Node* next;
+    }m_header;      // LinkList<T>在构造时会创建这个成员变量
+
+    int m_length;   // 链表长度
+
+    // 找到指向第 [index] 结点的前一个结点的指针
+    // 譬如：index==0 ==> ret==头结点； index==5 ==> ret==第[4]个结点的地址；
+    // 必须这样设计，因为插入、删除等操作都是需要 目标位置的 前一个结点配合的
+    // 而目标位置的 地址 则可以用 这个函数的返回值->next 确定；
+    // 若 return ret->next,这样返回的就是 [index]结点的地址，那么这样就只能得到
+    // 目标位置和目标位置的下一个结点的地址了，这样没法进行 插入、删除等操作。
+    Node* position(int index) const
+    {
+        // 由于const对象不能修改成员变量的值,对m_header取地址就
+        // 可能会修改m_header的值，可将m_header声明为mutable
+        Node* ret = reinterpret_cast<Node*>(&m_header);
+
+        // 最开始思维发生了一个恶心的错误，竟然认为for循环执行流是：
+        // pos=0 -> {}中代码 -> pos++ -> 判断pos<index -> {}代码
+        // 正确的应该是：
+        // pos=0 -> 判断pos<index -> {}中代码 -> pos++ -> 判断pos<index -> {}代码
+        for(int pos=0; pos < index; pos++)
+        {
+            ret = ret->next;
+        }
+
+        return ret;
+    }
+
+public:
+    LinkList()
+    {
+        m_header.next = NULL;
+        m_length = 0;
+    }
+
+    bool insert(int index, const T& element)  // 在任意位置头部插入
+    {
+        bool ret = ( (0 <= index) && (index <= m_length) );
+
+        if( ret )
+        {
+            Node* node = new Node();
+            node->value = element;
+
+            Node* current = position(index);
+
+            node->next = current->next;
+            current->next = node;
+
+            m_length++;
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Parameter index is invalid ...");
+        }
+
+        return ret;
+    }
+
+    bool insert(const T& element)   // 尾部插入
+    {
+        insert(m_length, element);
+    }
+
+    bool remove(int index)
+    {
+        bool ret = ( (0 <= index) && (index < m_length) );
+
+        if( ret )
+        {
+            Node* current = position(index);
+
+            Node* toDel = current->next;
+
+            current->next = toDel->next;
+
+            delete toDel;
+
+            m_length--;
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Parameter index is invalid ...");
+        }
+
+        return ret;
+    }
+
+    bool set(int index, const T& element)
+    {
+        bool ret = ( (0 <= index) && (index < m_length) );
+
+        if( ret )
+        {
+            (position(index)->next)->value = element;
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Parameter index is invalid ...");
+        }
+
+        return ret;
+
+    }
+
+    bool get(int index, T& element) const
+    {
+        bool ret = ( (0 <= index) && (index < m_length) );
+
+        if( ret )
+        {
+            element = (position(index)->next)->value;
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Parameter index is invalid ...");
+        }
+
+        return ret;
+    }
+
+    // 上面函数操作很不方便，重载一个这样的出来
+    T get(int index) const
+    {
+        T ret;
+
+        if( get(index, ret) )
+        {
+            return ret;
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Parameter index is invalid ...");
+        }
+    }
+
+    int length() const
+    {
+        return m_length;
+    }
+
+    void clear()
+    {
+        while(m_header.next)
+        {
+            Node* toDel = m_header.next;
+
+            m_header.next = toDel->next;
+
+            delete toDel;
+        }
+
+        m_length = 0;
+    }
+
+    ~LinkList()
+    {
+        clear();
+    }
+};
+
+}
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,62 +1,87 @@
-// main.cpp StaticArray类和DynamicArray类的使用示例
+// main.cpp LinkList类的使用示例
 #include <iostream>
-#include "DynamicArray.h"
-#include "StaticArray.h"
+#include "LinkList.h"
 
 using namespace std;
 using namespace WoodLib;
 
+class Test
+{
+public:
+    Test()
+    {
+        throw 0;  // 在构造函数中抛异常，故意制造出来的！
+    }
+};
+
 int main()
 {
-    DynamicArray<int> d1(5);
+    // 测试1
+    LinkList<int> list;
 
-    cout << "d1.length() = " << d1.length() << endl;
-
-    for(int i=0; i<d1.length(); i++)
+    for(int i=0; i<5; i++)
     {
-        d1[i] = i;
+        list.insert(i);
     }
 
-    for(int i=0; i<d1.length(); i++)
+    for(int i=0; i<list.length(); i++)
     {
-        cout << d1[i] << " ";
+        cout << list.get(i) << " ";
+    }
+
+    cout << endl;
+
+    list.remove(2);
+
+    list.set(0, 100);
+
+    for(int i=0; i<list.length(); i++)
+    {
+        cout << list.get(i) << " ";
     }
 
     cout << endl;
 
-    try
-    {
-        d1[8] = 1;
-    }
-    catch(const IndexOutOfBoundsException& e)
-    {
-        cout << e.getMessage() << endl;
-        cout << e.getLocation() << endl;
+    list.clear();
 
-        d1.resize(10);
-        d1[8] = 1;
+    for(int i=0; i<list.length(); i++)
+    {
+        cout << list.get(i) << " ";
     }
 
-    cout << "d1.length() = " << d1.length() << endl;
+    // 测试2
+    // 如果LinkList类不采用借尸还魂来解决Test抛异常的话，
+    // LinkList的使用者会以为这个异常是LinkList类设计的
+    // 问题，因为他们认为自己并没有调用Test的构造函数，所
+    // 以不会怀疑是Test类设计的问题，而是LinkList类的问题。
+    LinkList<Test> lt;
 
-    for(int i=0; i<d1.length(); i++)
-    {
-        cout << d1[i] << " ";
-    }
-
-    cout << endl;
+    Test t;  // 这样就会抛出异常了,这样就很明显与WoodLib库无关了
 
     return 0;
 }
 /* 运行结果
-d1.length() = 5
+Object::operator new: 12
+Object::operator new: 12
+Object::operator new: 12
+Object::operator new: 12
+Object::operator new: 12
 0 1 2 3 4
-Parameter index is invalid ...
-..\Array.h:61
 Object::~Object()
-d1.length() = 10
-0 1 2 3 4 64644 41405056 41353408 1 0
+Object::operator delete:0x2e710e8
+100 1 3 4
 Object::~Object()
+Object::operator delete:0x2e710b8
+Object::~Object()
+Object::operator delete:0x2e710d0
+Object::~Object()
+Object::operator delete:0x2e71100
+Object::~Object()
+Object::operator delete:0x2e71118
+
+This application has requested the Runtime to terminate it in an unusual way.
+Please contact the application's support team for more information.
+terminate called after throwing an instance of 'int'
 */
 
 
