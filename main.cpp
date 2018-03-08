1b010e0a2930ac328b218089827b00fd7b8186d1
Wood <liyao6@foxmail.com>
2018-03-08 22:57:52 +0800

    offsetof(TYPE, MEMBER)、container_of(MEMBER_POINTER, TYPE, MEMBER)宏的原理探析：   1、offsetof宏 利用0地址通过类型名、成员变量名求得成员变量相对于结构体的地址偏移量；   2、container_of宏 利用成员变量地址 - 该成员变量相对于结构体的地址偏移量 求得 结构体的地址；   3、typeof是GNU C编译器特有关键字，只在编译期有效用于得到变量的类型；   4、({})  GNU C编译器的语法扩展，与逗号表达式类似，结果为最后一个表达式的值；   5、container_of宏 在后面移植进WoodLib时为了保证通用性，去掉平台相关的代码部分，即去掉typeof和{()}，不过这样会失去对类型检查；

--- main.cpp
+++ main.cpp
@@ -1,65 +1,121 @@
-// main.cpp 循环单链表解决约瑟夫环问题
 #include <iostream>
-#include "DualLinkList.h"
+#include <stdio.h>
 
 using namespace std;
-using namespace WoodLib;
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t)&((TYPE*)0)->MEMBER)
+#endif
+
+// const typeof(...):编译期类型检查， const 指针兼容 const 和非 const 指针类型
+// typeof 根据变量名获取到这个变量的类型，这样第二部预处理阶段替换后就会在编译器有个类型检查
+// 又因为tryeof只在编译期有效运行期无效，所以也不会出现断错误；
+// 强制转为char*类型是为了做 指针运算
+#ifndef container_of
+#define container_of(ptr, type, member) ({                 \
+    const typeof(((type*)0)->member)* __mptr = (ptr);      \
+    (type*)((char*)__mptr - offsetof(type, member));})
+#endif
+
+struct ST
+{
+    int i;  // offset 0
+    int j;  // offset 4
+    char c; // offset 8
+};
+
+// ({})是何方神圣
+void test()
+{
+    int a = 0;
+    int b = 0;
+    int c = (a=1, b=2, a+b); // 括号表达式
+
+    // ({})表达式，其中的{}表示一个作用域， {}外再加上()表示取
+    // 最后一个语句的值功能上类似于逗号表达式，这是 GNU C 扩展语法
+    int d = ({int a=1; int b=2; a+b;});
+
+    printf("c = %d\n", c);
+    printf("d = %d\n", d);
+}
+
+// typeof 关键字：GNU C 的扩展关键字
+void type_of()
+{
+    int i = 100;
+    typeof(i) j = i;
+    const typeof(j)* p = &j;
+
+    printf("sizeof(j)= %d\n", sizeof(j));
+    printf("j = %d\n", j);
+    printf("*p = %d\n", *p);
+}
+
+void func(ST* pst)
+{
+    int* pi = &(pst->i);       // (unsigned int)pst + 0;
+    int* pj = &(pst->j);       // (unsigned int)pst + 4;
+    char* pc = &(pst->c);      // (unsigned int)pst + 8;
+
+    printf("pst = %p\n", pst);
+    printf("pi = %p\n", pi);
+    printf("pj = %p\n", pj);
+    printf("pc = %p\n", pc);
+}
 
 int main()
 {
-    DualLinkList<int> d1;
+    ST s = {0, 0, 0};
 
-    for(int i=0; i<5; i++)
-    {
-        d1.insert(i);
-        d1.insert(5);
-    }
+    func(&s);
+    func(NULL);  // 借用 0 地址计算成员变量的偏移地址，相当于 pst=0
+                 // 使用到 0 地址但为什么没有发生段错误了，因为这里
+                 // 并没有访问 0 地址的内容；
+                 // 编译器知道各个成员相对于结构体的地址偏移量是多少
+                 // 这里传个 0 地址就是利用了这点，0地址传递进func函数
+                 // 再在func函数中打印出各个结构体成员变量的地址
+                 // 而因为结构体地址是0,所以各个成员地址数值上就与
+                 // 这个成员的偏移量是一样的数值了
+                 // offsetof宏就是利用了这个原理
 
-    for(d1.move(0); !d1.isEnd(); d1.next())
-    {
-        cout << d1.current() << " ";
-    }
+    cout << "offset i: " << offsetof(ST, i) << endl;   // 0
+    cout << "offset j: " << offsetof(ST, j) << endl;   // 4
+    cout << "offset c: " << offsetof(ST, c) << endl;   // 8
 
-    cout << endl;
-    // 逆向访问链表
-    // 因为首结点的 prev 指针 和 尾结点的 next 指针都指向 NULL
-    // 所以可以都用!dl.end()作为循环退出条件的判断
-    for(d1.move(d1.length()-1); !d1.isEnd(); d1.pre())
-    {
-        cout << d1.current() << " ";
-    }
+    cout << "---------------------------" << endl;
 
-    cout << endl;
+    test();      // ({}) 用法测试
+    type_of();   // typeof 用法测试
 
-    // 删除值为 5 的元素
-    d1.move(d1.length() - 1);
-    while(!d1.isEnd())
-    {
-        if( 5 == d1.current() )
-        {
-            cout << d1.current() << " ";
-            d1.remove(d1.find(d1.current()));
-        }
-        else  // 因为 每进remove()一次就有对m_current指针修正一次
-        {
-            d1.pre();
-        }
-    }
+    ST s1 = {0, 0, 0};
 
-    cout << endl;
+    char* pc = &s1.c;
 
-    for(d1.move(0); !d1.isEnd(); d1.next())
-    {
-        cout << d1.current() << " ";
-    }
+    ST* pst = container_of(pc, ST, c);
 
-    cout << endl;
+    cout << "&s = " << &s1 << endl;
+    cout << "pst = " << pst << endl;
 
     return 0;
 }
 /* 运行结果
-0 5 1 5 2 5 3 5 4 5
-5 4 5 3 5 2 5 1 5 0
-5 5 5 5 5
-0 1 2 3 4
+pst = 0061fe88
+pi = 0061fe88
+pj = 0061fe8c
+pc = 0061fe90
+pst = 00000000
+pi = 00000000
+pj = 00000004
+pc = 00000008
+offset i: 0
+offset j: 4
+offset c: 8
+---------------------------
+c = 3
+d = 3
+sizeof(j)= 4
+j = 100
+*p = 100
+&s = 0x61fe7c
+pst = 0x61fe7c
 */
