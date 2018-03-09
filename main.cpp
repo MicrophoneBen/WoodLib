db12472f0735c650097ad4baceddc2a0014934c4
Wood <liyao6@foxmail.com>
2018-03-09 13:31:41 +0800

    栈的应用实例：成对符号匹配算法的设计要点：   1、当遇见左符号时压入栈中，当遇见对应右符号时进行匹配，恰好匹配就弹出栈顶元素；   2、匹配成功标志：所有字符扫描完毕，且栈为空；反之匹配失败或所有字符扫描完毕但栈非空则表达式不合法；   3、注意左操作符和右操作符一样时，这样的符合单独处理，注意这种情况的处理逻辑(扫描到发现栈为空或者栈顶不是这个扫描到的则说明这个是左操作符=>入栈;若栈顶元素与扫描到的相同，说明此时扫描到的是右操作符，则将栈顶元素弹出 )；   4、 不要忘记匹配成功栈也要完全弹出即为空栈；   5、从这个实例中可以看出，栈这种数据结构特别适用于用来解决 “就近匹配” 这类问题；   6、代码细节点：      字符表达式进行扫描检测匹配的算法函数中scanner()中，为了代码更严谨，注意不要忘记对传参 code 进行一个 NULL 的检测。

--- main.cpp
+++ main.cpp
@@ -1,69 +1,114 @@
-// LinkStack 基于链式存储结构栈（简称链式栈）的使用示例
+// 栈的应用实例：成对符号匹配问题
 #include <iostream>
 #include "LinkStack.h"
 
 using namespace std;
 using namespace WoodLib;
 
-class Test : public Object
+// 是否为左符号
+bool isLeft(char c)
 {
-public:
-    Test()
+    return (c == '(') || (c == '{') || (c == '[') || (c == '<');
+}
+
+// 是否为右符号
+bool isRight(char c)
+{
+    return (c == ')') || (c == '}') || (c == ']') || (c == '>');
+}
+
+// 是否为引号(单引号 双引号) 因为引号左右是一样的所以拿出来单独处理
+bool isQuot(char c)
+{
+    return (c == '\'') || (c == '\"');
+}
+
+// 匹配检测
+bool isMatch(char l, char r)
+{
+    return ((l=='(') && (r==')')) ||
+           ((l=='{') && (r=='}')) ||
+           ((l=='[') && (r==']')) ||
+           ((l=='<') && (r=='>')) ||
+           ((l=='\'') && (l==r))  ||
+           ((l=='\"') && (l==r));
+}
+
+// 核心代码：扫描字符，字符串都是以 '\0' 作为标志符结尾
+bool scanner(const char* code)
+{
+    bool ret = true;
+
+    LinkStack<char> stack;  // 链式栈
+    int i = 0;
+
+    code = (code == NULL) ? "" : code;  // 对传参进行NULL检测
+
+    // 1、开始按顺序读取字符，遇到结束符'\0' 或匹配失败时结束
+    while( ret && code[i] != '\0' )
     {
-        cout << "Test::Test()" << endl;
+        if( isLeft(code[i]) )
+        {
+            stack.push(code[i]);    // 左字符入栈
+        }
+        else if( isRight(code[i]) ) // 右字符
+        {
+            // 2.右字符是否与栈顶匹配
+            if( stack.size() > 0 && isMatch(stack.top(),code[i]) )
+            {
+                // 注意 栈空时出现右字符也是匹配失败的
+                stack.pop();        // 匹配，则出栈
+            }
+            else
+            {
+                ret = false;        // 匹配失败
+            }
+        }
+        else if( isQuot(code[i]) )  // 引号的处理
+        {
+            // 栈空时扫描到引号，说明此引号是左操作符，则当前字符入栈。
+            // 栈顶不是引号，也表示当前字符是左引号，则当前字符入栈。
+            if( stack.size() == 0 && !isMatch(stack.top(), code[i]) )
+            {
+                stack.push(code[i]);
+            }
+            else if( isMatch(stack.top(), code[i]) )
+            {
+                // 右引号（栈顶己经是引号了，表示当前字符(code[i])为右引号）
+                // 则此时弹出栈顶的引号
+                stack.pop();
+            }
+        }
+
+        i++;
     }
 
-    ~Test()
-    {
-        cout << "Test::~Test()" << endl;
-    }
-};
+    return ret && (stack.size() == 0);  // 不要忘记匹配成功栈也要完全弹出即为空
+}
 
 int main()
 {
-    LinkStack<Test> l1;    // 没有构造函数的调用
+    const char* s1 = "<a{b<c>(\'x\'\"\")d}e>";
+    const char* s2 = "#include <stdio.h>   \
+                      int main()          \
+                      {                   \
+                         int a[5][5];     \
+                         int (*p)[4];     \
+                         p = a[0];        \
+                         printf(\"%d\n\", &p[3][3] - &a[3][3]); \
+                         return 0;        \
+                      }";
 
-    cout << "l1.size() = " << l1.size() << endl << endl;
+    const char* s3 = "int a[5][5]][6]";
 
-    LinkStack<int> l2;
-
-    try
-    {
-        l2.pop();
-    }
-    catch(const Exception& e)
-    {
-        cout << e.getMessage() << endl;
-        cout << e.getLocation() << endl;
-    }
-
-    cout << "l2.push(): ";
-    for(int i=0; i<10; i++)
-    {
-        cout << i*i << " ";
-        l2.push(i*i);
-    }
-
-    cout << endl << "l2.size() = " << l2.size() << endl;
-
-    cout << "l2.pop(): ";
-    while( l2.size() > 0 )
-    {
-        cout << l2.top() << " ";
-        l2.pop();
-    }
-
-    cout << endl;
+    cout << "expression s1 : " << scanner(s1) << endl;
+    cout << "expression s2 : " << scanner(s2) << endl;
+    cout << "expression s3 : " << scanner(s3) << endl;
 
     return 0;
 }
-
 /* 运行结果
-l1.size() = 0
-
-No element in current stack ...
-..\LinkStack.h:37
-l2.push(): 0 1 4 9 16 25 36 49 64 81
-l2.size() = 10
-l2.pop(): 81 64 49 36 25 16 9 4 1 0
+expression s1 : 1
+expression s2 : 1
+expression s3 : 0
 */
