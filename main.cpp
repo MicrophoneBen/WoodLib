18315b0c9418e895776f0a8d77ee91f53cc3c19a
Wood <liyao6@foxmail.com>
2018-03-09 11:34:40 +0800

    StaticStack 顺序栈的严重缺陷：   1、问题：当存储的数据元素是类类型时，会随着对象的创建调用N次构造函数，销毁时N次析构函数，造成效率低下；   2、问题原因：栈的存储空间是用原生数组来存储的！   3、解决方法：链式栈解决！

--- main.cpp
+++ main.cpp
@@ -5,48 +5,60 @@
 using namespace std;
 using namespace WoodLib;
 
+class Test
+{
+public:
+    Test()
+    {
+        cout << "Test::Test()" << endl;
+    }
+
+    ~Test()
+    {
+        cout << "Test::~Test()" << endl;
+    }
+};
+
 int main()
 {
-    StaticStack<int, 10> s1;
+    StaticStack<Test, 10> s1;
 
-    cout << "s1.capacity() = " << s1.capacity() << endl;
+    // 顺序栈的严重缺陷，创建顺序栈对象时，若栈中存储的数据元素是类类型时
+    // 会调用N次构造函数，对象销毁时又会调用N次析构函数。明明没有压栈，但
+    // 却也会发生N次构造N次析构，这样效率是很底下的；
+
+    // 原因就是因为顺序栈是使用原生数组作为存储空间的，所以栈对象在创建时
+    // 数组的各个元素肯定也有个初始化的过程，当数组类型为类类型时自然就会
+    // 调用构造函数了，销毁时自然也就会调用析构函数了！
+
+    // 问题根源就在于顺序栈的这个存储空间会随着对象的创建而每个数组元素都
+    // 初始化一遍，从这入手，那么我用链表来作为存储空间这个问题不就迎刃而解
+    // 了吗！---> 这就是链式栈了！
+
     cout << "s1.size() = " << s1.size() << endl;
-
-    try
-    {
-        s1.pop();  // 此时栈为空 出栈自然会抛出异常
-    }
-    catch(const Exception& e)
-    {
-        cout << e.getMessage() << endl;
-        cout << e.getLocation() << endl;
-    }
-
-    cout << "s1.push: ";
-    for(int i=0; i<s1.capacity(); i++)
-    {
-        cout << i*i << " ";
-        s1.push(i*i);
-    }
-
-    cout << endl;
-
-    cout << "s1.pop(): ";
-    while(s1.size() > 0)
-    {
-        cout << s1.top() << " ";
-        s1.pop();
-    }
-
-    cout << endl;
 
     return 0;
 }
 /* 运行结果
-s1.capacity() = 10
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
+Test::Test()
 s1.size() = 0
-No element in current Stack ...
-..\StaticStack.h:62
-s1.push: 0 1 4 9 16 25 36 49 64 81
-s1.pop(): 81 64 49 36 25 16 9 4 1 0
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
+Test::~Test()
 */
