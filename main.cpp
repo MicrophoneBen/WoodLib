fc364721e1a2ac9ff79ea532f82c25b21e2350c8
Wood <liyao6@foxmail.com>
2018-03-13 02:05:45 +0800

    递归是一种数学模型应该用数学思维来看待：   1、递归是一种将问题分而自治的思想：分解后的问题与原问题的类型完全相同，但规模较小。通过对小规模问题的解，就轻易求得原问题的解；   2、递归一定要有递归出口，不能无限递归（栈冲爆）；   3、递归一定不要陷入递归函数的执行流的细节，要用一种数学思想来建立一个递归模型，再根据这个模型编写代码（编码注意递归出口）；   4、代码细节点：      mySrylen() 中 *str 的*号不要忘了；      mySrylen_beautiful() 精妙代码也都是基础代码反复优化得到的结果；      再次强调：递归用数学思想来理解，千万不要陷入执行流。

--- main.cpp
+++ main.cpp
@@ -1,66 +1,98 @@
-// main.cpp  String类新增加功能（基于KMP算法实现的）使用示例
+// main.cpp  数学思想来理解递归
 #include <iostream>
-#include "WoodString.h"
-#include "Exception.h"
+#include <cstdlib>
+#include <cstring>
 
 using namespace std;
-using namespace WoodLib;
+
+// 递归求和
+unsigned int sum(unsigned int n)
+{
+    unsigned int ret = 0;
+
+    if( n > 1 )
+    {
+        ret = n + sum(n - 1);
+    }
+
+    if( 1 == n )   // 递归出口
+    {
+        ret = 1;
+    }
+
+    return ret;
+}
+
+// 斐波拉契数列： 1,1,2,3,5,8,13,21,34,...
+// 注意小标设置为从1开始
+unsigned int fac(unsigned int index)
+{
+    unsigned int ret = 0;
+
+    if( index >= 3 )
+    {
+        ret = fac(index - 1) + fac(index - 2);
+    }
+
+    if( (index == 2) || (index == 1) )   // 递归出口
+    {
+        ret = 1;
+    }
+
+    return ret;
+}
+
+// 用递归求字符串长度
+unsigned int mySrylen(const char* str)
+{
+    unsigned int ret = 0;
+
+    // 注意是*str
+    if(*str != '\0')
+    {
+        // 1 + 跳过第0个 计算str下标1开始的字符串长度
+        ret = 1 + mySrylen(str + 1);
+    }
+    else
+    {
+        ret = 0;
+    }
+
+    return ret;
+}
+
+// 用一行代码改写上面递归求字符串长度的函数
+// 这个是想说明,在阅读他人代码时当看到他人很精妙的写法时不要惊讶
+// 这可能就是别人反复优化最后的代码而已,就如这里,实质原理与上面
+// 完全一样,但看上去却比上面优雅太多了
+// 同时这里也是想说明,基础理论很重要,对于下面这个如果不知道用数学
+// 思想来看待递归,那下面这个改写了一句程序绝对是看不懂的,上面普
+// 通写法还是可能看懂的,说明了基础理论的重要性
+unsigned int mySrylen_beautiful(const char* str)
+{
+    return str ? (*str ? 1 + mySrylen_beautiful(str + 1) : 0) : 0;
+}
 
 int main()
 {
-    String s = "Liyao learning DataStruct";
-    const char* c = "Li";
-    String p = "DataStruct";
+    cout << sum(100) << endl;
 
-    cout << " ------------- test 1 --------------- " << endl;
-    cout << s.find("Li") << endl;
-    cout << s.find(c) << endl;
-    cout << s.find(p) << endl;
+    cout << "[ ";
+    for(int i=1; i<=10; i++)    // 注意小标设置为从1开始
+    {
+        cout << fac(i) << " ";
+    }
+    cout << "]" << endl;
 
-    cout << " ------------- test 2 --------------- " << endl;
-    cout << s.remove("DataStruct").str() << endl;
+    cout << mySrylen("Liyao") << endl;
 
-    cout << " ------------- test 3 --------------- " << endl;
-    cout << (s + "DataStruct").str() << endl;
-    cout << s.str() << endl;
-    cout << (s += "DataStruct").str() << endl;
-    cout << s.str() << endl;
-
-    cout << " ------------- test 4 --------------- " << endl;
-    cout << s.replace("DataStruct","Qt").str() << endl;
-
-    cout << " ------------- test 5 --------------- " << endl;
-    cout << s.creatSubstr(0, 2).str() << endl;
-    cout << s.str() << endl;
-
-    cout << " ------------- test 6 --------------- " << endl;
-    cout << (s - "Qt").str() << endl;
-    cout << s.str() << endl;
-    cout << (s -= "Qt").str() << endl;
-    cout << s.str() << endl;
+    cout << mySrylen_beautiful("Liyao") << endl;
 
     return 0;
 }
 /* 运行结果
- ------------- test 1 ---------------
-0
-0
-15
- ------------- test 2 ---------------
-Liyao learning
- ------------- test 3 ---------------
-Liyao learning DataStruct
-Liyao learning
-Liyao learning DataStruct
-Liyao learning DataStruct
- ------------- test 4 ---------------
-Liyao learning Qt
- ------------- test 5 ---------------
-Li
-Liyao learning Qt
- ------------- test 6 ---------------
-Liyao learning
-Liyao learning Qt
-Liyao learning
-Liyao learning
+5050
+[ 1 1 2 3 5 8 13 21 34 55 ]
+5
+5
 */
