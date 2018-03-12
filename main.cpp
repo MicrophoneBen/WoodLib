775d0c3550146b06361508804681c5653feee9e9
Wood <liyao6@foxmail.com>
2018-03-12 19:51:57 +0800

    KMP 算法编码要点：   1、在目标字符串dest中查找子串substr，常规算法O(n) = len_dest * len_substr；KMP算法 O(n) = len_dest + len_substr；   2、常规算法匹配失败时移动一位再又逐个匹配，但大多数情况下移动一位比较是完全没有必要的但常规算法就是这么移动所以效率低下，KMP算法就是优化这个的；   3、KMP算法实现先知：substr匹配失败时的右移位数与子串本身相关，与目标串无关；   4、原理：移动位数 = substr已匹配的字符数 – 对应的部分匹配值，而任意子串都存在一个唯一的部分匹配表；   5、KMP算法编码要点：编码产生一个子串的部分匹配表PMT，再substr在匹配失败时就根据PMT表计算移动的位数；   6、编码生成PMT表：下标为 0 的元素 匹配值为 0 => 从 2 个字符开始递推（从下标为 1 的字符开始递推）=> 前缀与后缀向后拓展的字符相等时, 匹配值加1,将这个匹配值填入PMT表 => 当前缀与后缀向后拓展的字符不相等时, 匹配值到退至表中上一个元素值,直至再次判断相等，或者 匹配值 已经为 0 时；   7、KMP() 使用PMT表，在(substr[j] != dest[i])时，根据PMT表来移动指向substr的j（而不是像常规算法样每次移动一位再重新匹配），从substr的j位置再来与目标串dest匹配；   8、makePMT()返回堆指针，在KMP()中最后不要忘记free释放。   9、代码细节点：      常规算法findSubstr()中，第一个for循环次数只需要是 len_dest - len_substr 次，不需要len_dest次，这个地方注意理解，语言不好写；      makePMT() 中，for循环是从 1 开始，从第二个字符开始递推，第一个字符（即下标为0）匹配值肯定为0，for循环前直接写死了；      makePMT() 中，while()注意出口 PMT_element > 0; KMP()中也有类似做法 j > 0；      KMP() 计算ret这类值时注意用同一维度的计算就不容易出错了；      malloc()申请的堆内存是以字节为单位的，这点注意不要忘记乘以相应的类型所占的内存；

--- main.cpp
+++ main.cpp
@@ -1,50 +1,139 @@
-// main.cpp  String 字符串类使用示例
+// main.cpp  KMP 算法编码
 #include <iostream>
-#include "WoodString.h"
+#include "Exception.h"
+#include <cstdlib>
+#include <cstring>
 
 using namespace std;
 using namespace WoodLib;
 
-int main()
+// 在目标串dest中查找子串substr的常规算法，返回值为子串在目标串中的下标
+// O(n) = (len_dest-len_substr) * len_substr ==> len_dest * len_substr
+int findSubstr(const char* dest, const char* substr)
 {
-    String s = "abc1234";
+    int ret = -1;
 
-    cout << "s.length() = " << s.length() << endl;
+    int len_dest = strlen(dest);
+    int len_substr = strlen(substr);
+    int len = len_dest - len_substr;
 
-    cout << "s.startWith(\"abc\") : " <<  s.startWith("abc") << endl;
-    cout << "s.startWith(\"adc\") : " <<  s.startWith("adc") << endl;
-    cout << "s.endOf(\"234\") : " <<  s.endOf("234") << endl;
-    cout << "s.endOf(\"56\") : " <<  s.endOf("56") << endl;
-
-    for(int i=0; i < s.length(); i++)
+    for(int i=0; (ret<0) && (i<=len); i++)
     {
-        cout << s[i];
+        bool equal = true;
+
+        for(int j=0; equal && (j<len_substr); j++)
+        {
+            equal = equal && (dest[i + j] == substr[j]);
+        }
+
+        ret = (equal ? i : -1);
     }
 
-    cout << endl;
+    return ret;
+}
 
-    s.insert(0, "   Hello ");
-    s.insert(s.length(), " Liyao     ");
+// KMP算法： O(n) = len_dest + len_substr
+// 返回值：部分匹配表，在堆上申请的内存空间
+int* makePMT(const char* substr)
+{
+    int len = strlen(substr);
 
-    cout << "s.str() = [" << s.str() << "]" << endl;
-    cout << "s.length() = " << s.length() << endl;
+    // 不要忘记乘以 sizeof(int)
+    int* PMT = static_cast<int*>(malloc(sizeof(int) * len));
 
-    s.trim();
+    if( PMT != NULL )
+    {
+        int PMT_element = 0;  // 部分匹配表中的元素值
+        PMT[0] = 0;           // 1、下标为 0 的元素 匹配值为 0
 
-    cout << "s.str() = [" << s.str() << "]" << endl;
-    cout << "s.length() = " << s.length() << endl;
+        // 2、从 2 个字符开始递推（从下标为 1 的字符开始递推）
+        for(int i=1; i<len; i++)
+        {
+            // 4、当前缀与后缀向后拓展的字符不相等时, 匹配值到退至表中上一个元素值
+            // 直至再次 判断相等，或者 匹配值 已经为 0 时；
+            while( (PMT_element > 0) && (substr[PMT_element] != substr[i]) )
+            {
+                PMT_element = PMT[PMT_element - 1];
+            }
+
+            // 3、前缀与后缀向后拓展的字符相等时, 匹配值加1
+            if( substr[PMT_element] == substr[i] )
+            {
+                PMT_element++;
+            }
+
+            PMT[i] = PMT_element;  // 将部分匹配值填入表中
+        }
+    }
+
+    return PMT;
+}
+
+int KMP(const char* dest, const char* substr)
+{
+    int ret = -1;
+
+    int len_dest = strlen(dest);
+    int len_substr = strlen(substr);
+
+    int* PMT = makePMT(substr);
+
+    if( PMT != NULL )
+    {
+        for(int i=0,j=0; i<len_dest; i++)  // 目标串查找完毕跳出 for 循环
+        {
+            while( (j > 0) && (substr[j] != dest[i]) )
+            {
+                // 失配时，移动 j 到当前已经匹配了的子串的部分匹配值的位置
+                // 。如果仍然失配， j 继续照上面规则忘前移动， 直到匹配或 j
+                // 到达了子串开始的位置
+                j = PMT[j-1];
+            }
+
+            if(substr[j] == dest[i])
+            {
+                j++;
+            }
+
+            if( j == len_substr )  // 目标串中有子串,已经匹配完找到了时
+            {
+                ret = i + 1 - len_substr;  // 同一维度计算
+                break;
+            }
+        }
+    }
+    else
+    {
+        THROW_EXCEPTION(InvalidOperationException, "Nor exising normally PMT ...");
+    }
+
+    free(PMT);   // pmt 指向的是 make_pmt()申请的堆内存,所以得free
+                 // free释放其他函数中申请的堆内存,这个设计不好！！！
+                 // 能否改进一下？
+
+    return ret;
+}
+
+int main()
+{
+    cout << findSubstr("0123456789ABCDABCDABD", "ABCDABD") << endl << endl;   // 常规算法
+
+    int* PMT = makePMT("ABCDABD");       // PMT表
+
+    cout << "PMT = [ ";
+    for(int i=0; i<strlen("ABCDABD"); i++)
+    {
+        cout << PMT[i] << " ";
+    }
+    cout << "]" << endl;
+
+    cout << KMP("0123456789ABCDABCDABD", "ABCDABD") << endl;   // KMP算法
 
     return 0;
 }
 /* 运行结果
-s.length() = 7
-s.startWith("abc") : 1
-s.startWith("adc") : 0
-s.endOf("234") : 1
-s.endOf("56") : 0
-abc1234
-s.str() = [   Hello abc1234 Liyao     ]
-s.length() = 27
-s.str() = [Hello abc1234 Liyao]
-s.length() = 19
+14
+
+PMT = [ 0 0 0 0 1 2 0 ]
+14
 */
